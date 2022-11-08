f725314247a3df78a6330c3ee6f3f9c5bce64361
krishna Shahi <krishnashahi3888@gmail.com>
2022-11-08 19:39:50 +0000

    Parent to child non-primitive data communication

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -3,4 +3,15 @@ import { LightningElement,api} from 'lwc';
 export default class P2cParentComponent extends LightningElement {
     p_msg='Hurray!!! I got the data';
     @api p_cardHeading='Parent to child primitive data communication.'
+    carouselData=[
+        {src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+        header:  "First Card",
+        description:  "First description."},
+        {src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
+        header:  "Second Card",
+        description:  "Second description."},
+        {src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
+        header:  "Third Card",
+        description:  "Third description."}
+    ]
 }
