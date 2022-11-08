4bd0f48ae5ddba4e98015a6150f2329d643c25ea
krishna Shahi <krishnashahi3888@gmail.com>
2022-11-08 09:32:46 +0000

    Render method implementation

---
+++ force-app/main/default/lwc/renderMethod/renderMethod.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+import signinTemplate from './signinTemplate.html'
+import signupTemplate from './signupTemplate.html'
+import renderTemplate from './renderMethod.html'
+export default class RenderMethod extends LightningElement {
+    selectedBtn ='' 
+    render(){               
+        return this.selectedBtn ==='SignUp'? signupTemplate:
+               this.selectedBtn ==='SignIn'? signinTemplate:
+               renderTemplate
+    }
+
+    handleClick(event){
+        this.selectedBtn = event.target.label
+    }
+    handleSubmit(event){
+        console.log(`${event.target.label} Successfully!!`)
+    }
+}
